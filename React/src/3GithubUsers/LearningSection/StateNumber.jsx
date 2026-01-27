57098bc0284e2be852249e0eb0ed875dcdd79df7
ArthurcodeX <daniel.karanja239@gmail.com>
2026-01-27 05:56:43 +0300

    updated doc

--- React/src/3GithubUsers/LearningSection/StateNumber.jsx
+++ React/src/3GithubUsers/LearningSection/StateNumber.jsx
@@ -1,26 +1,46 @@
+import { useState } from "react";
+
 function StateNumber() {
+  const [n, setN] = useState(0); //call it returns an array []
+  //useState() ->[currentState,UpdateState:<set>]
+
+  const increment = () => {
+    console.log("Increment pressed");
+    let newV = n + 1;
+    setN(newV);
+  };
+  const decrement = () => {
+    console.log("Decrement Pressed");
+    let newV = n - 1;
+    setN(newV);
+  };
+
   return (
     <div className=" p-4 ">
-      <button
-        className=" m-2 rounded-full  shadow-md bg-red-600 text-white  font-bold active:opacity-50 cursor-pointer"
-        style={{
-          width: "20px",
-          height: "20px",
-        }}
-      >
-        -
-      </button>
-      <span>1</span>
-      <button
-        className=" m-2 rounded-full  shadow-md  bg-blue-600 text-white  font-bold active:opacity-50 cursor-pointer"
-        style={{
-          width: "20px",
-          height: "20px",
-        }}
-      >
-        +
-      </button>
+      <Button txt={"-"} color="red" actionFunc={decrement} />
+      <span>{n}</span>
+      <Button txt={"+"} color="blue" actionFunc={increment} />
     </div>
+  );
+}
+
+//DRY -> D not repeat
+
+function Button(props) {
+  const { txt, color, actionFunc } = props;
+
+  return (
+    <button
+      className=" m-2 rounded-full  shadow-md   text-white  font-bold active:opacity-50 cursor-pointer"
+      style={{
+        width: "20px",
+        height: "20px",
+        backgroundColor: color,
+      }}
+      onClick={actionFunc}
+    >
+      {txt}
+    </button>
   );
 }
 
--- React/src/3GithubUsers/index.jsx
+++ React/src/3GithubUsers/index.jsx
@@ -1,11 +1,13 @@
 import UsersSection from "./UsersSection";
 import SearchSection from "./SearchSection";
 import StateNumber from "./LearningSection/StateNumber";
+import Colors from "./LearningSection/Colors";
 
 function App() {
   return (
     <div>
       <StateNumber />
+      <Colors />
       <SearchSection />
       <UsersSection />
     </div>
